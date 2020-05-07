2da1a5822ec1978036a63302c95a00add262a290
Danilo Silva <danilocgsilva@gmail.com>
2020-05-06 22:18:10 -0300

    WIP: Setup added
    First work has done in setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="add_vhost_5",
+    version=VERSION,
+    description="Creates a virtual host in the local development machine",
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+    keywords="VirtualHost development environment local apache",
+    url="https://github.com/danilocgsilva/add_vhost_5",
+    author="Danilo Silva",
+    author_email="contact@danilocgsilva.me",
+    packages=["add_vhost_5"],
+    entry_points={"console_scripts": ["avhost=add_vhost_5.__main__:main"],},
+    include_package_data=True
+)
